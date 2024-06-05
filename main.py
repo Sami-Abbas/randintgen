8f440272994aa1967f166b582828cd3cdf8df60e
Sami-Abbas <sami_abbas09@outlook.com>
2024-06-05 22:00:28 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
+from flask import Flask
+from prometheus_client import start_http_server, Counter
 import random
 import time
-from prometheus_client import start_http_server, Gauge
-start_http_server(8000)
-random_number_gauge = Gauge('random_number', 'A random number between 100 and 1000')
-while True:
-    number=random.randint(100,1000)
-    random_number_gauge.set(number)
-    print(number)
-    time.sleep(10)
+
+app = Flask(__name__)
+
+# Define a Prometheus counter metric
+random_number_counter = Counter('random_number', 'Random number generated')
+
+@app.route('/generate')
+def generate_random_number():
+    random_number = random.randint(100, 1000)
+    random_number_counter.inc()  # Increment the counter
+    return f"Random number generated: {random_number}"
+
+if __name__ == '__main__':
+    # Start the Prometheus HTTP server
+    start_http_server(9090)
+    # Start the Flask app
+    app.run(host='0.0.0.9090', port=9090)
